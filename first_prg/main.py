d7e64a8c52e25c431751912cd519ad3fcbad6a3b
Andre <a@a.ru>
2025-11-25 19:53:03 +0300

    add new print

--- first_prg/main.py
+++ first_prg/main.py
@@ -1,9 +1,12 @@
 # Add comments to this function
 def print_hi(text):
+    print('--------------------------------------------')
     print(f'Hi, {text}')
+    print('--------------------------------------------')
 
 # Test change program and send to Git
 if __name__ == '__main__':
     print_hi('Its my first script for send it to Git!')
 
-#This new text line
+# This new text line
+# Local add new comments line
